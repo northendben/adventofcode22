63ce2e1d17a301c42a68378240245cf2d53ca199
northendben <75284984+northendben@users.noreply.github.com>
2022-12-13 14:22:18 -0800

    Add files via upload

---
+++ Python/Advent of Code Day 5 Part 2.py
@@ -0,0 +1,58 @@
+import re
+import pdb
+with open("Day5input.txt") as file:
+    data=file.read().replace("    ", "None,").replace("]",",").replace("[","").replace(" ","").split("\n")
+
+
+key_list = []
+format_list = []
+for line in data:
+    x = line.split(",")
+    format_list.append(x)
+keys = format_list.pop()
+key_list= [int(char) for char in keys[0]]
+stack_dict = dict.fromkeys(key_list,None)
+for item in stack_dict:
+    stack_dict[item] = []
+
+final_list = []
+for stack in format_list:
+    hold_list = []
+    for count,box in enumerate(stack,start=1):
+        if box != "None" and box != "":
+            stack_dict[count].insert(0,box)
+
+
+with open("Day5instructions.txt", "r") as file:
+    data=file.read().split("\n")
+instructions = []
+regex = re.compile(r'(\d+)')
+for item in data:
+    matches = regex.findall(item)
+    for count,item in enumerate(matches):
+        matches[count] = int(item)
+    instructions.append(matches)
+
+
+for item in instructions:
+    # print(stack_dict)
+    # pdb.set_trace()
+    hold_list = []
+    count = 0
+    box_count = item[0]
+    from_stack = item[1]
+    to_stack = item[2]
+    while count < box_count:
+        box_being_moved = stack_dict[from_stack].pop()
+        hold_list.insert(0,box_being_moved)
+        count+=1
+    for box in hold_list:
+        stack_dict[to_stack].append(box)
+        
+print(stack_dict)
+answer = [char[-1] for char in list(stack_dict.values())]
+answer = "".join(answer)
+print(answer)
+
+{1: ['N', 'C', 'R', 'T', 'M', 'Z', 'P'], 2: ['D', 'N', 'T', 'S', 'B', 'Z'], 3: ['M', 'H', 'Q', 'R', 'F', 'C', 'T', 'G'], 4: ['G', 'R', 'Z'], 5: ['Z', 'N', 'R', 'H'], 6: ['F', 'H', 'S', 'W', 'P', 'Z', 'L', 'D'], 7: ['W', 'D', 'Z', 'R', 'C', 'G', 'M'], 8: ['S', 'J', 'F', 'L', 'H', 'W', 'Z', 'Q'], 9: ['S', 'Q', 'P', 'W', 'N']}
+
